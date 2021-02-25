69afc6d8baa921588241cf0ad8e6d6af946f529d
korede <khodedng@yahoo.com>
2021-02-26 00:32:54 +0100

    add error handler for APIs and promises,request query and parameter filter

---
+++ pkg/utilities/pick.js
@@ -0,0 +1,17 @@
+/**
+ * Selecting data from request and query paramaters
+ * @param {Object} object
+ * @param {string[]} keys
+ * @returns {Object}
+ */
+const pick = (object, keys) => {
+  return keys.reduce((obj, key) => {
+    if (object && Object.prototype.hasOwnProperty.call(object, key)) {
+      // eslint-disable-next-line no-param-reassign
+      obj[key] = object[key];
+    }
+    return obj;
+  }, {});
+};
+
+module.exports = pick;
