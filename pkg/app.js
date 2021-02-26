c4ff7ecf0d49315e10c3995c194c8c0f35b60e22
korede <khodedng@yahoo.com>
2021-02-26 01:07:06 +0100

    initialized node server, fixed incompatible mongoose version with node 14 by downgrading mongoose, fixed husky config

---
+++ pkg/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mongoSanitize = require('express-mongo-sanitize');
+const httpStatus = require('http-status');
+const config = require('./config/config');
+const morgan = require('./config/morgan');
+const { errorConverter, errorHandler } = require('./middlewares/error');
+const ApiError = require('./utilities/ApiError');
+
+const app = express();
+
+if (config.env !== 'test') {
+  app.use(morgan.successHandler);
+  app.use(morgan.errorHandler);
+}
+
+// parse json request body
+app.use(express.json());
+
+// parse urlencoded request body
+app.use(express.urlencoded({ extended: true }));
+
+// sanitize request data
+app.use(mongoSanitize());
+
+// send back a 404 error for any unknown api request
+app.use((req, res, next) => {
+  next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
+});
+
+// convert error to ApiError, if needed
+app.use(errorConverter);
+
+// handle error
+app.use(errorHandler);
+
+module.exports = app;
